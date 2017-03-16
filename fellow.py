272fc0e50f36221827e736c4b7219c1ade245c80
Maryanne <maryanne.nganga@andela.com>
2017-03-16 11:05:43 +0300

    Adds print allocations function and fixes reallocate room.

--- fellow.py
+++ fellow.py
@@ -2,5 +2,5 @@ from person import Person
 
 class Fellow (Person):
     def __init__(self, person_id, name):
-        super().__init__(person_id, name)
+        super(Fellow, self).__init__(person_id, name)
         
--- office.py
+++ office.py
@@ -2,4 +2,4 @@ from room import Room
 class Office(Room):
 
     def __init__(self, name, no_of_members = 0 , max_members =6):
-        super().__init__(name, max_members, no_of_members=0)
+        super(Office, self).__init__(name, max_members, no_of_members=0)
